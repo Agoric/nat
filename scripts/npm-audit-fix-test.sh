7ab2170876d75e1bbabc3cf76c381955b0184814
AgoricBot <kate+agoricbot@agoric.com>
2019-03-19 15:05:23 -0700

    add scripts --kate not bot

---
+++ scripts/npm-audit-fix-test.sh
@@ -0,0 +1,13 @@
+git checkout -b npm-audit-fix
+
+npm audit --fix
+
+touch 'testfile'
+
+git add . 
+
+git commit -m "results of running npm audit --fix"
+
+git push -u origin npm-audit-fix
+
+hub pull-request --no-edit -r @katelynsills -l automated,npm-audit-fix
