744d957f9f92b7251b71394aef51b7d50696547d
AgoricBot <kate+agoricbot@agoric.com>
2019-03-19 14:58:46 -0700

    results of running [90m                                                                                [39m [90m [39m                      === npm audit security report ===                       [90m [39m [90m                                                                                [39m found 0 vulnerabilities  in 501 scanned packages

---
+++ scripts/npm-audit-fix.sh
@@ -0,0 +1,23 @@
+if npm audit ; then
+    echo "Nothing to fix"
+else
+    git config user.email "kate+agoricbot@agoric.com"
+    git config user.name "AgoricBot"
+
+    set -xe
+    go get -u -d github.com/github/hub
+    cd /go/src/github.com/github/hub
+    go install github.com/github/hub
+
+    git checkout -b npm-audit-fix
+
+    npm audit --fix
+
+    git add . 
+
+    git commit -m "results of running `npm audit --fix`"
+
+    git push -u origin npm-audit-fix
+
+    hub pull-request --no-edit -r katelynsills -l automated,npm-audit-fix
+fi
