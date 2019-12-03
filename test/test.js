e428e66ab3728b4b683086cb7791f91760032df2
Kate Sills <kate@agoric.com>
2019-12-03 14:34:23 -0800

    add eslint settings to handle BigInt, run lint-fix

--- test/test.js
+++ test/test.js
@@ -8,7 +8,7 @@ test('Nat() works for natural BigInts', t => {
   t.equal(Nat(BigInt('1')), BigInt('1'));
   t.equal(Nat(BigInt('1')), 1n);
   t.equals(Nat(BigInt(2 ** 53)), BigInt(2 ** 53));
-  t.equal(Nat(9007199254741000n), 9007199254741000n)
+  t.equal(Nat(9007199254741000n), 9007199254741000n);
   t.end();
 });
 
@@ -22,14 +22,23 @@ test('Nat() throws for non-BigInts', t => {
   t.throws(() => Nat(1), /TypeError: 1 is not a BigInt/);
   t.throws(() => Nat(999), /TypeError: 999 is not a BigInt/);
   t.throws(() => Nat(3.0), /TypeError: 3 is not a BigInt/);
-  t.throws(() => Nat('not a number'), /TypeError: not a number is not a BigInt/);
+  t.throws(
+    () => Nat('not a number'),
+    /TypeError: not a number is not a BigInt/,
+  );
   t.throws(() => Nat(-1), /TypeError: -1 is not a BigInt/);
   t.throws(() => Nat(0.5), /TypeError: 0.5 is not a BigInt/);
-  t.throws(() => Nat(2 ** 60), /TypeError: 1152921504606847000 is not a BigInt/);
+  t.throws(
+    () => Nat(2 ** 60),
+    /TypeError: 1152921504606847000 is not a BigInt/,
+  );
   t.throws(() => Nat(NaN), /TypeError: NaN is not a BigInt/);
   t.throws(() => Nat(Infinity), /TypeError: Infinity is not a BigInt/);
   t.throws(() => Nat('3'), /TypeError: 3 is not a BigInt/);
   t.throws(() => Nat(3.1), /TypeError: 3.1 is not a BigInt/);
-  t.throws(() => Nat(2 ** 53 - 1), /TypeError: 9007199254740991 is not a BigInt/);
+  t.throws(
+    () => Nat(2 ** 53 - 1),
+    /TypeError: 9007199254740991 is not a BigInt/,
+  );
   t.end();
 });
